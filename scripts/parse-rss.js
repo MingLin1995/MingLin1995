ce1348c16c34b67d11216f3ddebd362f7295fc2a
Ming <125284928+MingLin1995@users.noreply.github.com>
2024-07-16 16:27:17 +0800

    Update parse-rss.js

--- scripts/parse-rss.js
+++ scripts/parse-rss.js
@@ -13,7 +13,7 @@ const MARKER_END = '<!-- BLOG-POST-LIST:END -->'; // 文章列表結束的標記
   feed.items.slice(0, 5).forEach(item => {
     newContent += `- [${item.title}](${item.link})\n`;
   });
-  newContent += `${MARKER_END}\n`;
+  newContent += `${MARKER_END}`;
 
   const originalReadme = fs.readFileSync(README_PATH, 'utf8');
   const updatedReadme = originalReadme.replace(new RegExp(`${MARKER_START}[\\s\\S]*${MARKER_END}`), newContent);
